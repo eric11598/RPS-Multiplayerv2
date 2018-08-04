53bf59e98bed853dd034d8e9e86fbd2a72daed8d
eric11598 <eric.arellano.0803@gmail.com>
2018-08-04 19:39:03 -0400

    change players

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -34,6 +34,11 @@ $("#add-user").on("click", function(event) {
   event.preventDefault();
   name = $("#name-input").val().trim();
 
+  console.log("THIS IS PLAYER "+player);
+
+
+
+
 
   dataRef.ref().child("players").update({
     [name]: {
@@ -45,114 +50,112 @@ $("#add-user").on("click", function(event) {
             status: 'connected',
           }});
     
-        $("#inputContainer").hide();
+  $("#inputContainer").hide();
 
-          console.log("BOOO");
-          dataRef.ref().update({
-            turn: 1,
-          });
+
+
+
+
+
+  
+  dataRef.ref().update({
+    turn: 1, 
+  });
       
 
     });
     
 
-  $(".btn-secondary").on("click", function(event) {
+$(".btn-secondary").on("click", function(event) {
+
+  var div = "#player"+this.id+"ButtonContainer";
+        
+  var gameTurn;
+  var move = $(this).val();
+  console.log("yeeet ya yeeet");
       
-      
-      var gameTurn;
-      var move = $(this).val();
+  dataRef.ref().child("players").child(name).update({
+    move: move,          
+  });
 
-      
-      dataRef.ref().child("players").child(name).update({
-            move: move,          
-          });
-
-      ref.once("value", function(snapshot) {
-        gameTurn = snapshot.val().turn;
-      });
+  ref.once("value", function(snapshot) {
+    gameTurn = snapshot.val().turn;
+  });
 
 
-      if (gameTurn === 1)
-      gameTurn = 2;
+  //If 2 inputs are recorded, start the game
 
-      else if (gameTurn === 2)
-      {
-        console.log("here");
-        var playerOne = {};
-        var playerTwo = {};
+  if (gameTurn === 1)
+    gameTurn = 2;
+
+  else if (gameTurn === 2) {
+    
+    var playerOne = {};
+    var playerTwo = {};
 
         //OPTIMIZE WITH FOR LOOPS 
 
-        playersRef.on('value', function(snapshot){
-          snapshot.forEach(function(child){
-              console.log("here");
-              var key = child.key;
+    playersRef.on('value', function(snapshot) {
+      snapshot.forEach(function(child) {
+        console.log("here");
+        var key = child.key;
 
 
-              var moveValue = JSON.stringify(child.val().move);
-              var numberValue = JSON.stringify(child.val().playerNumber);
-              var nameValue = JSON.stringify(child.val().playerName);
-              var winsValue = JSON.stringify(child.val().wins);
-              var lossesValue = JSON.stringify(child.val().losses);
+        var moveValue = JSON.stringify(child.val().move);
+        var numberValue = JSON.stringify(child.val().playerNumber);
+        var nameValue = JSON.stringify(child.val().playerName);
+        var winsValue = JSON.stringify(child.val().wins);
+        var lossesValue = JSON.stringify(child.val().losses);
 
-              numberValue = numberValue.replace(/['"]+/g, '')
-              moveValue = moveValue.replace(/['"]+/g, '')
-              nameValue = nameValue = nameValue.replace(/['"]+/g, '')
+        numberValue = numberValue.replace(/['"]+/g, '')
+        moveValue = moveValue.replace(/['"]+/g, '')
+        nameValue = nameValue = nameValue.replace(/['"]+/g, '')
 
-              if(numberValue === 'One')
-              {
-                playerOne['move'] = moveValue;
-                playerOne['wins'] = Number(winsValue);
-                playerOne['losses'] = Number(lossesValue);
-                playerOne['name'] = nameValue;
-                playerOne['number'] = numberValue;
-              }
+        if(numberValue === 'One')
+        {
+          playerOne['move'] = moveValue;
+          playerOne['wins'] = Number(winsValue);
+          playerOne['losses'] = Number(lossesValue);
+          playerOne['name'] = nameValue;
+          playerOne['number'] = numberValue;
+        }
 
-              if(numberValue === 'Two')
-              {
-                playerTwo['move'] = moveValue;
-                playerTwo['wins'] = Number(winsValue);
-                playerTwo['losses'] = Number(lossesValue);
-                playerTwo['name'] = nameValue;
-                playerTwo['number'] = numberValue;
-              }
+        if(numberValue === 'Two')
+        {
+          playerTwo['move'] = moveValue;
+          playerTwo['wins'] = Number(winsValue);
+          playerTwo['losses'] = Number(lossesValue);
+          playerTwo['name'] = nameValue;
+          playerTwo['number'] = numberValue;
+        }
+            
+      });   
+    });
+  
+      // ROCK PAPER SCISSORS LOGIC
 
+      var winner;
 
-
-              
-              //user[playerNumber] = value;
-          });
-
-          
-      });
-        console.log(playerOne);
-        console.log(playerTwo);
-
-        
-        var winner;
-
-        
-
-        if(playerOne.move === playerTwo.move)
+      if(playerOne.move === playerTwo.move)
         winner = 0;
 
         
-        if(playerOne.move==='rock' && playerTwo.move === 'paper')
+      if(playerOne.move==='rock' && playerTwo.move === 'paper')
         winner = 2;
 
-        if(playerOne.move==='rock' && playerTwo.move === 'scissors')
+      if(playerOne.move==='rock' && playerTwo.move === 'scissors')
         winner = 1;
 
-        if(playerOne.move==='paper' && playerTwo.move === 'rock')
+      if(playerOne.move==='paper' && playerTwo.move === 'rock')
         winner = 1;
 
-        if(playerOne.move==='paper' && playerTwo.move === 'scissors')
+      if(playerOne.move==='paper' && playerTwo.move === 'scissors')
         winner = 2;
 
-        if(playerOne.move==='scissors' && playerTwo.move === 'rock')
+      if(playerOne.move==='scissors' && playerTwo.move === 'rock')
         winner = 2;
 
-        if(playerOne.move==='scissors' && playerTwo.move === 'paper')
+      if(playerOne.move==='scissors' && playerTwo.move === 'paper')
         winner = 1;
         
 
@@ -187,9 +190,7 @@ $("#add-user").on("click", function(event) {
         playerOne.losses = losses;
       }
 
-      console.log(playerOne);
-      console.log(playerTwo);
-        
+
         gameTurn = 1;
 
         dataRef.ref().child("players").update({
@@ -219,26 +220,49 @@ $("#add-user").on("click", function(event) {
         turn: gameTurn,
       });
 
+  
+  console.log(div);
+  $(div).hide();   
 
-    });
+});
+
+playersRef.on('value', function(snapshot){
+  snapshot.forEach(function(child){
+
+    var numberValue = JSON.stringify(child.val().playerNumber);
+    var winsValue = JSON.stringify(child.val().wins);
+    var lossesValue = JSON.stringify(child.val().losses);
+
+    numberValue = numberValue.replace(/['"]+/g, '')
+
+    var winsDiv = "#player"+numberValue+"Wins";
+    var lossesDiv = "#player"+numberValue+"Losses";
+
+
+  
+    $(winsDiv).text(winsValue);
+    $(lossesDiv).text(lossesValue);
+
+  });
+});
+
+//Removes player and turn reference on disconnect/refresh from Firebase
+
+$( window ).unload(function() {
+  
+  var playerReference = "players/"+name;
+  var playerRef = firebase.database().ref(playerReference)
+  var turnRef = firebase.database().ref("turn");
+  
+  playerRef.onDisconnect().remove();
+  turnRef.onDisconnect().remove();
+
+
+});
     
 
 
-    $( window ).unload(function() {
-
-      var playerReference = "players/"+name;
-
-      var playerRef = firebase.database().ref(playerReference)
-      var turnRef = firebase.database().ref("turn");
-
-      playerRef.onDisconnect().remove();
-      turnRef.onDisconnect().remove();
-
-    });
-    
-
-
-    playersRef.once('value', function(snapshot) {
+playersRef.once('value', function(snapshot) {
       if (snapshot.hasChild('one')) {
         console.log("check once");
         $("#playerOneName").text(snapshot.val().players.name.playerName);
@@ -246,40 +270,50 @@ $("#add-user").on("click", function(event) {
     });
 
     
-    //If player 1 drops out and player 2 exists, next player is player 1
-    ref.child("players").orderByChild("playerNumber").equalTo("Two").once("value", function(snapshot){
-      if (snapshot.exists()){
+//If player 1 drops out and player 2 exists, next player is player 1
+
+ref.child("players").orderByChild("playerNumber").equalTo("Two").once("value", function(snapshot){
+  if (snapshot.exists()){
         player = "One";
         console.log("exists!");
       }
-    });
+  });
 
    
-    //If no players, first player to enter is player 1, second is player 2
-    //If a first player exists, second player is still player 2
-    playersRef.on("value", function(snapshot) {
-      
+//If no players, first player to enter is player 1, second is player 2
+//If a player exists, determine which player it is
 
-      if(snapshot.exists())
-      { 
-        player = 'Two';
-      }
-      else
-      {
-        player = 'One';
-      }
-    
-    });
+playersRef.on('value', function(snapshot){
+
+  var playerCount = 0;
+  var foundPlayer = '';
+
+  player = 'Two';
+
+
+  snapshot.forEach(function(child){
+
+    foundPlayer = JSON.stringify(child.val().playerNumber);
+
+    if (foundPlayer === 'Two')
+    player = 'One';
+
+    playerCount++;
+
+  });
+
+  if (playerCount === 0)
+    player = 'One';
+
+
+});
+
   
-    ref.on("value", function(snapshot) {
+ref.on("value", function(snapshot) {
 
       if(childNumber == 2)
       { 
         var gameTurn = snapshot.val().turn;
-        console.log("START GAMMEEE BOOOYS" + gameTurn);
-
-        
-
 
         
         playersRef.child(name).once("value", function(snapshot){
@@ -312,42 +346,48 @@ $("#add-user").on("click", function(event) {
 
     });
   
+
+
+
+
+
+
   // 3. Create Firebase event for adding employee to the database and a row in the html when a user adds an entry
   
   
-  playersRef.on("child_added", function(childSnapshot) {
-  
-    childNumber++;
+playersRef.on("child_added", function(childSnapshot) {
     
-    var number = childSnapshot.val().playerNumber;
-    var div = "#player"+number+"Name";
-    $(div).text(childSnapshot.val().playerName);
-
-
-    var key = childSnapshot.key; // "ada"
-
+  childNumber++;
     
-  
-  });
+  var number = childSnapshot.val().playerNumber;
+  var div = "#player"+number+"Name";
+  $(div).text(childSnapshot.val().playerName);
 
-  playersRef.on("child_removed", function(childSnapshot) {
+  var key = childSnapshot.key; // "ada"
+
+  var winsDiv = "#player"+number+"Wins";
+  var lossesDiv = "#player"+number+"Losses";
   
-    childNumber--;
+  $(winsDiv).text("0");
+  $(lossesDiv).text("0");
+
+});
+
+playersRef.on("child_removed", function(childSnapshot) {
+  
+  childNumber--;
     
-    var number = childSnapshot.val().playerNumber;
-    var div = "#player"+number+"Name";
-    console.log(div);
-    $(div).text("Waiting...");
+  var number = childSnapshot.val().playerNumber;
+  var div = "#player"+number+"Name";
+   
+  $(div).text("Waiting...");
 
+  var winsDiv = "#player"+number+"Wins";
+  var lossesDiv = "#player"+number+"Losses";
   
-  });
+  $(winsDiv).text("0");
+  $(lossesDiv).text("0");
 
-  
-  // Example Time Math
-  // -----------------------------------------------------------------------------
-  // Assume Employee start date of January 1, 2015
-  // Assume current date is March 1, 2016
-  
-  // We know that this is 15 months.
-  // Now we will create code in moment.js to confirm that any attempt we use meets this test case
+});
+
   
